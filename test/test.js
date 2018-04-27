cee01c2d8f674d108cf35784bcc3da4783d6172e
jiangtao <jtaobox@126.com>
2018-04-27 23:34:59 +0800

    修改单元测试；

--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
 // add.test.js
 const VelocityAsset = require('./../VelocityAsset')
 const path = require('path')
-
+const Parser = require('parcel-bundler/src/Parser')
 describe('VelocityAsset', function() {
-  it('velocity asset file parse', async function() {
-    const asset = new VelocityAsset(path.join(__dirname, './test.vm'), undefined, { rootDir: path.join(__dirname) })
-    console.log((await asset.process()))
+  it('velocity asset file parse', function() {
+    const asset = new VelocityAsset(path.join(__dirname, './test.vm'), {  }, { 
+      rootDir: path.join(__dirname), 
+      parser: new Parser(),
+      publicURL: '/' + path.basename('dist')
+    })
+    asset.process().then(res => {
+      if (~res.indexOf('jiangtao')) {
+        // console.log('ok')
+      }
+    })
   })
 })
